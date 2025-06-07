d36f013dca1e25390ee0d8b5820f8c44b1a2e238
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-06-06 20:48:11 -0500

    no message

--- source/uniandes/cupi2/simuladorBancario/interfaz/InterfazSimulador.java
+++ source/uniandes/cupi2/simuladorBancario/interfaz/InterfazSimulador.java
@@ -90,7 +90,7 @@ public class InterfazSimulador extends JFrame
         setSize( 600, 580 );
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-        cuenta = new SimuladorBancario( "50.152.468", "Sergio López" );
+        cuenta = new SimuladorBancario( "1.099.735.735", "Nicolás Arrieta" );
 
         panelCDT = new PanelCDT( this );
         panelAhorros = new PanelAhorros( this );
